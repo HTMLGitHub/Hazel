59dcc99ff0628aa4dc1d1386f309d7fbcf9f1ad4
Matthew <bak120405@gmail.com>
2022-02-01 20:31:37 -0500

    Hazel Game Engine Begin 2/1/2022

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//For use by Hazel Applications
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+// ---EntryPoint ------------
+#include "Hazel/EntryPoint.h"
+//---------------------------
---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Hazel
+{
+    class HAZEL_API Application
+    {
+    public:
+        Application();
+        virtual ~Application();
+        void Run();
+    };
+
+    //To be definded in Client
+    Application* CreateApplication();
+}
+
