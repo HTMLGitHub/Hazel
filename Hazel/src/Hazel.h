9164120461fc438e12973f6bbdc7c5a531db8d0f
Matthew <bak120405@gmail.com>
2022-02-02 19:19:36 -0500

    Logging 2-2-2022

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 //For use by Hazel Applications
 #include <stdio.h>
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ---EntryPoint ------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,13 @@
 extern Hazel::Application* Hazel::CreateApplication();
     int main(int argc, char** argv)
     {
+        Hazel::Log::Init();
+        HZ_CORE_WARN("Warning: Core Log Initialized!\n");
+        
+        int a = 5;
+
+        HZ_CLIENT_INFO("Hello! Var={0}", a);
+
         printf("Hazel Engine\n");
         auto app = Hazel::CreateApplication();
         app->Run();
